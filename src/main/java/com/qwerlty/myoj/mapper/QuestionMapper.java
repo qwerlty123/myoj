ed9c6f324ab06e0f526e28081522d0f7b1151a98
qwerlty123 <1787129184@qq.com>
2026-02-21 17:47:07 +0800

    初步增删改查

---
+++ src/main/java/com/qwerlty/myoj/mapper/QuestionMapper.java
@@ -0,0 +1,18 @@
+package com.qwerlty.myoj.mapper;
+
+import com.qwerlty.myoj.model.entity.Question;
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+
+/**
+* @author 17871
+* @description 针对表【question(题目)】的数据库操作Mapper
+* @createDate 2026-02-20 22:22:51
+* @Entity com.myoj.model.entity.Question
+*/
+public interface QuestionMapper extends BaseMapper<Question> {
+
+}
+
+
+
+
---
+++ src/main/java/com/qwerlty/myoj/mapper/QuestionSubmitMapper.java
@@ -0,0 +1,18 @@
+package com.qwerlty.myoj.mapper;
+
+import com.qwerlty.myoj.model.entity.QuestionSubmit;
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+
+/**
+* @author 17871
+* @description 针对表【question_submit(题目提交)】的数据库操作Mapper
+* @createDate 2026-02-20 22:25:35
+* @Entity com.myoj.model.entity.QuestionSubmit
+*/
+public interface QuestionSubmitMapper extends BaseMapper<QuestionSubmit> {
+
+}
+
+
+
+
